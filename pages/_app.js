91812759ac7745a8e24cb9828dfcaa6cf254f761
Rahil <rahilcrypto@protonmail.com>
2022-04-05 14:38:46 +0200

    main

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import {ThirdwebWeb3Provider} from '@3rdweb/hooks'
+
+const supportedChainIDs = [4]
+const connectors = {
+  injected: {},
+}
 
 function MyApp({ Component, pageProps }) {
+  return (
+  <ThirdwebWeb3Provider
+  supportedChainIDs={supportedChainIDs}
+  connectors={connectors}
+  >
   return <Component {...pageProps} />
+  </ThirdwebWeb3Provider>
+  )
 }
 
 export default MyApp
